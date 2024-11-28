51f27405fe272b4fa89dfbe1cebb25816af388b8
Ashley Young <asyoung@redhat.com>
2024-11-27 20:22:50 -0500

    global table data added to setup script.

--- models/setup_db.py
+++ models/setup_db.py
@@ -1,4 +1,6 @@
+from models import coaches, locations, insert_data, equipment
 from models.cyberconnect2 import engine
+from datetime import datetime, timezone
 from .base import Base
 
 # import table models to be created
@@ -30,14 +32,30 @@ from .t_events import Event_Test, Events_Competition, Event_Performance
 from .t_journal import Journal_Notes, Journal_Videos
 
 
+date_created = datetime.now(timezone.utc)
+
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
-    # we know that some tables need default starting data
-    # so lets populate those
-    # IceType should have some default sessions listed
-    # Boots, Blades, Config should contain at least a rental setting
-    # Locations table should have at least the rinks in the user's state
-    # uSkaterConfig should have a nearby rink suggested
+    # we know that some tables need default starting data for all users
+    ice_types = [
+        {'type_id': 1, 'ice_type': 'Free Style (Walk On)'},
+        {'type_id': 2, 'ice_type': 'Public'},
+        {'type_id': 3, 'ice_type': 'Coaching Session'},
+        {'type_id': 4, 'ice_type': 'Competition'},
+        {'type_id': 5, 'ice_type': 'Performance'},
+        {'type_id': 6, 'ice_type': 'Club Ice'},
+        {'type_id': 7, 'ice_type': 'Group Class'},
+        {'type_id': 8, 'ice_type': 'Free Style (Punch Card)'},
+        {'type_id': 9, 'ice_type': 'Off Ice - Concrete or Asphalt'},
+        {'type_id': 10, 'ice_type': 'Off Ice - Rink'}
+    ]
+
+    locations.add_types(ice_types)
+
+    coaches.add_coaches([{'coach_Fname': '-', 'coach_Lname': '-', 'coach_rate': '0', 'uSkaterUUID': 0}])
 
 # to create, run: python -m models.setup_db
+
+# this script is a complete mess. PEP8 complains about the imports, which are all over the place.
+# note sure how to address this, if you're reading this - I sincerely wish it was better. :)
