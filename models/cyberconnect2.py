ea591f22dff951fbb4f98ec4aaffca1e0c696bfc
Ashley Young <asyoung@redhat.com>
2024-11-29 16:02:53 -0500

    updating cyberconnect2 to be smater about env vars

--- models/cyberconnect2.py
+++ models/cyberconnect2.py
@@ -3,10 +3,10 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 
-db_url = os.environ['pgdb_host']
-db_name = os.environ['pgdb_name']
-db_user = os.environ['pgdb_user']
-passwd = os.environ['pgdb_password']
+db_url = os.environ.get('pgdb_host', -1)
+db_name = os.environ.get('pgdb_name', -1)
+db_user = os.environ.get('pgdb_user', -1)
+passwd = os.environ.get('pgdb_password', -1)
 
 engine = create_engine(f'postgresql://{db_user}:{passwd}@{db_url}/{db_name}')
 Session = sessionmaker(bind=engine)
